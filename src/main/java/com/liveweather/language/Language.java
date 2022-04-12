a6bda796c90027ed64a20c8f9361ef9b90150faf
LT_Name <572413378@qq.com>
2022-04-12 21:42:44 +0800

    chs

--- src/main/java/com/liveweather/language/Language.java
+++ src/main/java/com/liveweather/language/Language.java
@@ -1,6 +1,7 @@
 package com.liveweather.language;
 
 import cn.nukkit.Server;
+import cn.nukkit.utils.Config;
 import com.liveweather.check.Lang;
 import com.liveweather.storage.Options;
 
@@ -12,11 +13,14 @@ public class Language {
     String location = Server.getInstance().getFilePath() + "/plugins/" + "LiveWeather" + "/" + "jarfile/lang";
     String DE = "/de.cfg";
     String EN = "/en.cfg";
+    String CHS = "/chs.properties";
     File de = new File(location + DE);
     File en = new File(location + EN);
+    Config chs = new Config(location + CHS, Config.PROPERTIES);
     public String get(String obj) {
         String toreturn = "";
-        if(new Options().getConfig("language").toLowerCase().equals("en")) {
+        String language = new Options().getConfig("language");
+        if(language.equalsIgnoreCase("en")) {
             try {
                 Scanner myReader = new Scanner(en);
                 while (myReader.hasNextLine()) {
@@ -30,7 +34,9 @@ public class Language {
             } catch (FileNotFoundException e) {
                 return obj;
             }
-        }else{
+        }else if (language.equalsIgnoreCase("chs")) {
+            return chs.getString(obj, obj);
+        }else {
             try {
                 Scanner myReader = new Scanner(de);
                 while (myReader.hasNextLine()) {
